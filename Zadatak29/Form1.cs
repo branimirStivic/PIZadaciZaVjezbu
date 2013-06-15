1f40e1bddb3963d25dd722dc159cd8348827ab63
Branimir Štivić <shtiva@hotmail.com>
2013-06-16 00:42:59 +0200

    Zadaci djelomično -31

---
+++ Zadatak29/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadatak29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            List<int> brojevi = new List<int>();
+            int random = 0;
+            for (int i = 0; i <8; i++)
+            {
+                do
+                {
+                    random = r.Next(1, 39);
+                }
+                while (brojevi.Contains(random)); 
+                brojevi.Add(random);
+            }
+
+            label1.Text = brojevi[0].ToString();
+            label2.Text = brojevi[1].ToString();
+            label3.Text = brojevi[2].ToString();
+            label4.Text = brojevi[3].ToString();
+            label5.Text = brojevi[4].ToString();
+            label6.Text = brojevi[5].ToString();
+            label7.Text = brojevi[6].ToString();
+
+        }
+    }
+}
---
+++ Zadatak33/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadatak33
+{
+    public partial class Form1 : Form
+    {
+        int r = 0, g = 0, b = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            r = trackBar1.Value;
+            this.BackColor = Color.FromArgb(r, g, b);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+           g = trackBar2.Value;
+            this.BackColor = Color.FromArgb(r, g, b);
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            b = trackBar3.Value;
+            this.BackColor = Color.FromArgb(r, g, b);
+        }
+    }
+}
