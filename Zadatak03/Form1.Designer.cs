49e653ce494fe08d984badc894c0e3ed75ccd128
Branimir Štivić <shtiva@hotmail.com>
2013-06-15 02:15:03 +0200

    Zadatad03

--- Zadatak03/Form1.Designer.cs
+++ Zadatak03/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnUhvati = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUhvati
+            // 
+            this.btnUhvati.Location = new System.Drawing.Point(88, 114);
+            this.btnUhvati.Name = "btnUhvati";
+            this.btnUhvati.Size = new System.Drawing.Size(103, 58);
+            this.btnUhvati.TabIndex = 0;
+            this.btnUhvati.Text = "Uhvati me!";
+            this.btnUhvati.UseVisualStyleBackColor = true;
+            this.btnUhvati.Click += new System.EventHandler(this.btnUhvati_Click);
+            this.btnUhvati.MouseEnter += new System.EventHandler(this.btnUhvati_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(295, 263);
+            this.Controls.Add(this.btnUhvati);
+            this.Name = "Form1";
+            this.Text = "bu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnUhvati;
     }
 }
 
--- Zadatak03/Form1.cs
+++ Zadatak03/Form1.cs
@@ -10,11 +10,30 @@ using System.Windows.Forms;
 
 namespace Zadatak03
 {
+   
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnUhvati_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnUhvati_MouseEnter(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int xPozicija, yPozicija;
+            xPozicija = rand.Next(200);
+            yPozicija = rand.Next(200);
+            //MessageBox.Show(xPozicija.ToString());
+           
+            this.btnUhvati.Location = new Point(xPozicija, yPozicija);
         }
     }
 }
